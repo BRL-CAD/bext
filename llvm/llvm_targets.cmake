40819ab18906a619bac9df7ebd59bdedf508393e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2023-09-21 15:00:52 -0400

    Explain why this particular setting is in a cache file

--- llvm/llvm_targets.cmake
+++ llvm/llvm_targets.cmake
@@ -1,1 +1,5 @@
+# This setting is here because semicolon separated lists are a problem
+# in ExternalProject_Add - they just get interpreted as separate arguments
+# rather than a list.
+
 set(LLVM_TARGETS_TO_BUILD "ARM;X86" CACHE STRING "Enabled backends")
